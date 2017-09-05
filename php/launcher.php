1d8daea05e6e2395e23e1ae7c8531328efbab38e
nick <nick@Neptune.solent.ac.uk>
2017-09-05 11:54:40 +0100

    launcher script now deletes logfiles in the tmp directory

--- php/launcher.php
+++ php/launcher.php
@@ -15,23 +15,31 @@ $_SESSION["ephpuser"] = "ephp001";
 $user = null;
 
 if(isset($_SESSION["ephpuser"]) &&
-		preg_match("/^ephp\d{3}$/", $_SESSION["ephpuser"])) {
+        preg_match("/^ephp\d{3}$/", $_SESSION["ephpuser"])) {
 
-	$cmd = (isset($_POST["cmd"]) && $_POST["cmd"]=="stop") ? "stop":
-			(isset($argv[1]) && $argv[1]=="stop" ? "stop":"");
+    $cmd = (isset($_POST["cmd"]) && $_POST["cmd"]=="stop") ? "stop":
+            (isset($argv[1]) && $argv[1]=="stop" ? "stop":"");
 
-	$stopping = $cmd=="stop";
+    $stopping = $cmd=="stop";
     $scripts = ["wsocksrv", "xdclient"];
 
     for($i=($stopping ? count($scripts)-1 : 0); 
-		$i!=($stopping ?  -1: count($scripts));
-		$i+=($stopping ? -1:1)) {
+        $i!=($stopping ?  -1: count($scripts));
+        $i+=($stopping ? -1:1)) {
         $cmd1="/usr/bin/php ".SCRIPTDIR."{$scripts[$i]}.php $cmd > ".
                 TMPDIR."{$scripts[$i]}_out.txt &";
-		system($cmd1);
+        system($cmd1);
         usleep(500000);
     }
-	$user = $_SESSION["ephpuser"];
+    if($stopping) {
+        $files=glob("/var/www/tmp/*_out.txt");
+        foreach($files as $file) {
+            if(is_file($file)) {
+                unlink($file);
+            }
+        }
+    }
+    $user = $_SESSION["ephpuser"];
 }
 echo json_encode(["user"=>$user]);
 ?>
