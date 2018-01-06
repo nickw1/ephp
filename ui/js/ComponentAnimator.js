c8e146b7f2a054891aa8306b9d5b3d4167b43edf
Nick Whitelegg <nickw4426@gmail.com>
2018-01-06 07:43:47 +0000

    Added isAlteredState flag to prevent unwanted behaviour when window resized during component animation

--- ui/js/ComponentAnimator.js
+++ ui/js/ComponentAnimator.js
@@ -17,84 +17,93 @@ function ComponentAnimator (totalTime, interval, minWidth, elemIds) {
     this.minWidth = minWidth;
 
     this.timer = null;
-	this.ignored = null;
+    this.ignored = null;
+
+    this.isAlteredState = false;
 }
 
 ComponentAnimator.prototype.startForwardAnim = function(cb) {
     if(this.timer == null) {
-    	this.recalculateDimensions();
+        this.recalculateDimensions();
         this.timer = setInterval(this.doAnim.bind(this, 1, cb), this.interval);
+        this.isAlteredState = true;
     }
 }
 
 ComponentAnimator.prototype.startReverseAnim = function(cb) {
-    if(this.timer == null) {
-        this.timer = setInterval(this.doAnim.bind(this, -1, cb), this.interval);
-        for(var i=0; i<this.elem.length; i++) {
-			if(this.elem[i] != this.ignored) {
-            	this.elem[i].style.display = 'inline-block';
-			}
-        }
-    }    
+    if(this.midSteps[1] < 0.0001) {
+        console.log('WARNING! midSteps is 0');
+    } else {
+        if(this.timer == null) {
+            this.timer = setInterval(this.doAnim.bind(this, -1, cb), 
+                this.interval);
+            for(var i=0; i<this.elem.length; i++) {
+                if(this.elem[i] != this.ignored) {
+                    this.elem[i].style.display = 'inline-block';
+                }
+            }
+        }        
+    }
 }
 
 // offseWidth includes border; without this the animation won't work
 // correctly
 ComponentAnimator.prototype.doAnim = function (direction, cb) {
 
-	console.log("midSteps=" + this.midSteps[1]);
+    console.log("midSteps=" + this.midSteps[1]);
     var grower = direction==-1 ? this.elem[0] : this.elem[this.elem.length-1];
     var shrinker = direction==-1 ? this.elem[this.elem.length-1]: this.elem[0];
 
     shrinker.fullResizeWidth(shrinker.offsetWidth-this.endsStep);
     var growerWidth = grower.offsetWidth + this.endsStep;
     for(var i=1; i<this.elem.length-1; i++) {
-		if(this.elem[i] != this.ignored) {
-			console.log("ofset width="+ this.elem[i].offsetWidth);
-				console.log("Setting with to: 	" + (this.elem[i].offsetWidth-
-						(this.midSteps[i]*direction)));
-        	this.elem[i].fullResizeWidth
-            	(this.elem[i].offsetWidth- (this.midSteps[i]*direction));
-			console.log("now ofset width="+ this.elem[i].offsetWidth +
-				" midsteps=" + this.midSteps[i] + " direction="+direction);
-		}
-	
+        if(this.elem[i] != this.ignored) {
+            console.log("ofset width="+ this.elem[i].offsetWidth);
+                console.log("Setting with to:     " + (this.elem[i].offsetWidth-
+                        (this.midSteps[i]*direction)));
+            this.elem[i].fullResizeWidth
+                (this.elem[i].offsetWidth- (this.midSteps[i]*direction));
+            console.log("now ofset width="+ this.elem[i].offsetWidth +
+                " midsteps=" + this.midSteps[i] + " direction="+direction);
+        }
+    
         growerWidth += this.midSteps[i];
     }
     grower.fullResizeWidth(growerWidth);
-	
+    
     if(document.getElementById(shrinker.id+"_img")) {
         document.getElementById(shrinker.id+"_img").width = 
             (shrinker.offsetWidth> 75 ? 75: shrinker.offsetWidth);
     }
     for(var i=1; i<this.elem.length-1; i++) {
-		if(this.elem[i] != this.ignored) {
-        	if(document.getElementById(this.elem[i].id+"_img")) {
-            	document.getElementById(this.elem[i].id+"_img").width = 
-            	(this.elem[i].offsetWidth> 75 ? 75: this.elem[i].offsetWidth);
-        	}
-			
-		}
+        if(this.elem[i] != this.ignored) {
+            if(document.getElementById(this.elem[i].id+"_img")) {
+                document.getElementById(this.elem[i].id+"_img").width = 
+                (this.elem[i].offsetWidth> 75 ? 75: this.elem[i].offsetWidth);
+            }
+            
+        }
     }
 
     if(direction==1 && this.elem[0].offsetWidth<this.minWidth) {
         this.elem[0].fullResizeWidth(this.minWidth);
         for(var i=1; i<this.elem.length-1; i++) {
-			if(this.elem[i] != this.ignored) {
-            	this.elem[i].fullResizeWidth(this.minWidth* 
-                	(this.origWidths[i]/this.origWidths[0]));
-			}
+            if(this.elem[i] != this.ignored) {
+                this.elem[i].fullResizeWidth(this.minWidth* 
+                    (this.origWidths[i]/this.origWidths[0]));
+            }
         }
         this.finishAnim(cb);
     } else if (direction==-1 && this.elem[0].offsetWidth> this.origWidths[0]) {
         this.elem[0].fullResizeWidth(this.origWidths[0]);
         for(var i=1; i<this.elem.length-1; i++) {
-			if(this.elem[i] != this.ignored) {
-            	this.elem[i].fullResizeWidth(this.origWidths[i]);
-			}
+            if(this.elem[i] != this.ignored) {
+                this.elem[i].fullResizeWidth(this.origWidths[i]);
+            }
         }
         this.elem[this.elem.length-1].fullResizeWidth
             (this.origWidths[this.origWidths.length-1]);
+        this.isAlteredState = false;
         this.finishAnim(cb);
     }
 }
@@ -108,17 +117,19 @@ ComponentAnimator.prototype.finishAnim = function (cb) {
 }
 
 ComponentAnimator.prototype.recalculateDimensions = function() {
-    this.endsStep = Math.round((this.elem[0].offsetWidth-this.minWidth) / 
+    if(!this.isAlteredState) {
+        this.endsStep = Math.round((this.elem[0].offsetWidth-this.minWidth) / 
             this.nSteps);
-    for(var i=0; i<this.elem.length; i++) {
-        this.origWidths[i] = this.elem[i].offsetWidth;
-        if(i>=1 && i<this.elem.length-1) {
-            this.midSteps[i]=this.endsStep*
-            (this.elem[i].offsetWidth/this.elem[0].offsetWidth);
+        for(var i=0; i<this.elem.length; i++) {
+            this.origWidths[i] = this.elem[i].offsetWidth;
+            if(i>=1 && i<this.elem.length-1) {
+                this.midSteps[i]=this.endsStep*
+                (this.elem[i].offsetWidth/this.elem[0].offsetWidth);
+            }
         }
     }
 }
 
 ComponentAnimator.prototype.setIgnored = function(elem) {
-	this.ignored = elem;
+    this.ignored = elem;
 }
